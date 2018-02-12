e2fd217abee230ac6644c729d3a4b24780a14d03
Aleksey Cheusov <vle@gmx.net>
2018-02-12 15:58:00 +0300

    C-based classias2svmlight: tests were added

---
+++ classias2svmlight_JudyHS/test.mk
@@ -0,0 +1,43 @@
+ds = ${SRCDIR_datasets}
+output = ${.OBJDIR}/output.txt
+
+test: all
+	@set -e; \
+	\
+	export PATH=${.OBJDIR:Q}:$$PATH; \
+	\
+	echo 'Test #1'; \
+	classias2svmlight < ${ds}/small_train.classias \
+	    > ${output}; \
+	diff -u expect1.out ${output}; \
+	echo '      succeeded'; \
+	\
+	echo 'Test #2'; \
+	env HSVM_LABEL_MAP='zero:0 one:1 two:2' \
+	    classias2svmlight < ${ds}/small_train.classias \
+	    > ${output}; \
+	diff -u expect2.out ${output}; \
+	echo '      succeeded'; \
+	\
+	echo 'Test #3'; \
+	env HSVM_LABEL_MAP='one:1 two:2' \
+	    classias2svmlight < ${ds}/small_train.classias \
+	    2> ${output} >/dev/null || true; \
+	diff -u expect3.out ${output}; \
+	echo '      succeeded'; \
+	\
+	echo 'Test #4.1'; \
+	env HSVM_LABEL_MAP='one:1 two:zzz' \
+	    classias2svmlight < ${ds}/small_train.classias \
+	    2> ${output} >/dev/null || true; \
+	diff -u expect4.out ${output}; \
+	echo '      succeeded'; \
+	\
+	echo 'Test #4.2'; \
+	env HSVM_LABEL_MAP='one:1 two: zero:0' \
+	    classias2svmlight < ${ds}/small_train.classias \
+	    2> ${output} >/dev/null || true; \
+	diff -u expect4.out ${output}; \
+	echo '      succeeded'
+
+CLEANFILES +=	${output}
